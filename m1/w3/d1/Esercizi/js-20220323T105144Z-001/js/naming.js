cfd4de4281b94c744b0e0a8a1d9ebb1ab8f7f3de
francyfrattini <107913694+francyfrattini@users.noreply.github.com>
2022-07-04 16:53:10 +0200

    w3-d1-esercizio

---
+++ m1/w3/d1/Esercizi/js-20220323T105144Z-001/js/naming.js
@@ -0,0 +1,29 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+
+var petPreferito = 'criceto';
+console.log(petPreferito);
+
+var PetPreferito = 'coniglio';
+console.log(PetPreferito);
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+var criceto= 'Gino'
+console.log(criceto)
+
+var coniglio= 'Roger'
+console.log(coniglio)
+
+console.log(criceto + ' è il mio ' + petPreferito + ', ' + coniglio + ' è il mio ' + PetPreferito)
+
+
+//var 12 = 'numero';
